ea802b67b5050a4f4ad64a820ad2a2c188252ca3
Emir Demirli <commandiron@gmail.com>
2022-09-03 23:29:56 +0300

    code optimization

--- app/src/main/java/com/commandiron/vacationchecklist/domain/use_cases/InsertAllChecklistItems.kt
+++ app/src/main/java/com/commandiron/vacationchecklist/domain/use_cases/InsertAllChecklistItems.kt
@@ -7,18 +7,13 @@ class InsertAllChecklistItems(
     private val repository: Repository
 ) {
     suspend operator fun invoke(vacation: Vacation){
-        println(commonChecklistItems.size)
-        println(summerVacationChecklistItems.size)
-        println(skiTripChecklistItems.size)
         when(vacation.id){
             0 -> {
                 val checklistItems = commonChecklistItems + summerVacationChecklistItems
-                println("1: " + checklistItems.size)
                 repository.insertChecklistItems(checklistItems)
             }
             1 -> {
                 val checklistItems = commonChecklistItems + skiTripChecklistItems
-                println("2: " + checklistItems.size)
                 repository.insertChecklistItems(checklistItems)
             }
         }
