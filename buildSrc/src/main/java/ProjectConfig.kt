7721f2173e8081fec9ad1053c419a840db28341e
Emir Demirli <commandiron@gmail.com>
2022-09-20 16:57:55 +0300

    version.

--- buildSrc/src/main/java/ProjectConfig.kt
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -3,6 +3,6 @@ object ProjectConfig {
     const val compileSdk = 32
     const val minSdk = 23
     const val targetSdk = 32
-    const val versionCode = 7
-    const val versionName = "1.0.6"
+    const val versionCode = 8
+    const val versionName = "1.0.7"
 }
