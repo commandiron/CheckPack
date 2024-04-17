29e8acbb890a9b6e3980c8b155e8bb421fd4b3f6
commandiron <commandiron@gmail.com>
2024-04-17 23:17:09 +0300

    .

--- buildSrc/src/main/java/ProjectConfig.kt
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -2,7 +2,7 @@ object ProjectConfig {
     const val appId = "com.commandiron.vacationchecklist"
     const val compileSdk = 32
     const val minSdk = 23
-    const val targetSdk = 32
+    const val targetSdk = 33
     const val versionCode = 8
     const val versionName = "1.0.7"
 }
